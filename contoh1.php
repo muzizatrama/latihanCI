96f96e8fb04792f090baae7c786ea12b5e82d6c4
muzizatrama <muzizatramadan19@gmail.com>
2021-09-29 14:46:20 +0700

    first commit

---
+++ contoh1.php
@@ -0,0 +1,21 @@
+<?php
+{
+		echo "<h1> Perkenalkan</h1>";
+		echo "Nama Saya : Muzizat Ramadan
+
+		Nim saya: 12200555<?php
+
+
+		Jurusan saya: Sistem Informasi
+
+		Saya kuliah di: BSI
+
+		Saya tinggal di daerah: Bogor
+
+		Hobby saya adalah: Main Basket";
+
+
+	}
+
+
+
---
+++ zatizat/contoh1.php
@@ -0,0 +1,25 @@
+<?php
+class contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1> Perkenalkan</h1>";
+		echo "Nama Saya : Muzizat Ramadan
+
+		Nim saya: 12200555<?php
+		
+
+		Jurusan saya: Sistem Informasi
+
+		Saya kuliah di: BSI
+
+		Saya tinggal di daerah: Bogor
+
+		Hobby saya adalah: Main Basket";
+
+
+	}
+
+
+
+}
